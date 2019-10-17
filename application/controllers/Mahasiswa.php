4876025b410a29b9adaa3ae9ee3b126ae3d56d71
Ichlasul Al <m.ichal7749@mail.unpas.ac.id>
2019-10-17 21:36:35 +0700

    membuat form tambah data dengan code igniter

---
+++ application/controllers/Mahasiswa.php
@@ -0,0 +1,31 @@
+<?php
+
+class Mahasiswa extends CI_Controller
+{
+    public function __construct()
+    {
+        parent::__construct();
+        $this->load->model('Mahasiswa_model');
+        $this->load->library('form_validation');
+    }
+    public function index()
+    {
+        $data['mahasiswa'] = $this->Mahasiswa_model->getAllMahasiswa();
+        $data['judul'] = 'Daftar Mahasiswa';
+        $this->load->view('templates/header', $data);
+        $this->load->view('mahasiswa/index', $data);
+        $this->load->view('templates/footer');
+    }
+
+    public function tambah()
+    {
+        $data['judul'] = 'Form Tambah Data Mahasiswa';
+        if ($this->form_validation->run() == FALSE) {
+            $this->load->view('templates/header', $data);
+            $this->load->view('mahasiswa/tambah');
+            $this->load->view('templates/footer');
+        } else {
+            echo "ok";
+        }
+    }
+}
---
+++ application/views/mahasiswa/tambah.php
@@ -0,0 +1,49 @@
+<div class="container">
+
+    <div class="row mt-3">
+        <div class="col-md-6">
+
+
+            <div class="card">
+                <div class="card-header">
+                    Tambah Data Mahasiswa
+                </div>
+                <div class="card-body">
+                    <form action="" method="post">
+                        <div class="form-group">
+                            <label for="nama">Nama</label>
+                            <input type="text" class="form-control" id="nama">
+                        </div>
+                        <div class="form-group">
+                            <label for="nrp">NRP</label>
+                            <input type="text" class="form-control" id="nrp">
+                        </div>
+                        <div class="form-group">
+                            <label for="email">Email</label>
+                            <input type="text" class="form-control" id="email">
+                        </div>
+                        <div class="form-group">
+                            <label for="jurusan">Jurusan</label>
+                            <select class="form-control" id="jurusan" name="jurusan">
+                                <option value="Teknik Informatika">Teknik Informatika</option>
+                                <option value="Teknik Industri">Teknik Industri</option>
+                                <option value="Teknik Mesin">Teknik Mesin</option>
+                                <option value="Teknik lingkungan">Teknik lingkungan</option>
+                                <option value="Teknologi Pangan ">Teknologi Pangan </option>
+                                <option value="Teknik Planologi">Teknik Planologi</option>
+                            </select>
+                        </div>
+                        <button type="submit" name="tambah" class="btn btn-primary float-right">Tambah Data</button>
+
+                    </form>
+                </div>
+            </div>
+
+
+
+
+
+        </div>
+    </div>
+
+</div>
