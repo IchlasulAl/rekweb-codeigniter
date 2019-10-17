c669e66794756c32beaf21282ca8eb197ede9183
Ichlasul Al <m.ichal7749@mail.unpas.ac.id>
2019-10-17 22:21:58 +0700

    menambahkan fungsi tambah data ke database

--- application/models/Mahasiswa_model.php
+++ application/models/Mahasiswa_model.php
@@ -6,4 +6,14 @@ class Mahasiswa_model extends CI_model
     {
         return $this->db->get('mahasiswa')->result_array();
     }
+
+    public function tambahDataMahasiswa()
+    {
+        $data=[
+            "nama"=> $this->input->post('nama'),
+            "nrp"=> $this->input->post('nrp'),
+            "email"=> $this->input->post('email'),
+            "jurusan"=> $this->input->post('jurusan')
+        ];
+    }
 }
