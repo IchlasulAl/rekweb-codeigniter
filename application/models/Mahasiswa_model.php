bd5d528b9052a7465b82e8851ce13207444b6288
Ichlasul Al <m.ichal7749@mail.unpas.ac.id>
2019-10-17 23:00:57 +0700

    menambahan fitur hapus untuk data di database

--- application/models/Mahasiswa_model.php
+++ application/models/Mahasiswa_model.php
@@ -9,11 +9,19 @@ class Mahasiswa_model extends CI_model
 
     public function tambahDataMahasiswa()
     {
-        $data=[
-            "nama"=> $this->input->post('nama'),
-            "nrp"=> $this->input->post('nrp'),
-            "email"=> $this->input->post('email'),
-            "jurusan"=> $this->input->post('jurusan')
+        $data = [
+            "nama" => $this->input->post('nama', true),
+            "nrp" => $this->input->post('nrp', true),
+            "email" => $this->input->post('email', true),
+            "jurusan" => $this->input->post('jurusan', true)
         ];
+
+        $this->db->insert('mahasiswa', $data);
+    }
+
+    public function hapusDataMahasiswa($id)
+    {
+        $this->db->where('id',$id);
+        $this->db->delete('mahasiswa');
     }
 }
